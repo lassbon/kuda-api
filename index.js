9a97c6dcbd6ce9bda09f16b8e7f10572e2852810
rosh <roshbon@gmail.com>
2022-10-26 14:49:20 +0100

    added sequelize

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.APP_PORT
+const  sequelize  = require('./config/sequelize')
+const bodyParser = require('body-parser')
+
 
 
 
 app.listen(port, () => {
 
-    console.log(`Example app listening on port ${port}`)
+    sequelize.authenticate()
+    .then(sucessData => {
+            console.log('Connection has been established successfully.');
+    })
+    .catch(error => {
+            console.error('Unable to connect to the database:', error);
+    })
     
 })
