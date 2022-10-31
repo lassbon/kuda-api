319698febf1c0665c7bf0bce4b1b91de294d4f9c
rosh <roshbon@gmail.com>
2022-10-31 14:33:23 +0100

    used sequelize to create model and migrations

---
+++ models/otp.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const { Model } = require('DataTypes');
+
+module.exports = (sequelize, DataTypes) => {
+	class otp extends Model {
+		
+	}
+	customer.init(
+        {
+            sn: { 
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            
+            },
+            customer_id: { 
+                type: DataTypes.STRING,    
+            },
+            otp: { 
+                type: DataTypes.STRING,
+            
+            },
+            phone: { 
+                type: DataTypes.STRING,
+            
+            },
+            email: { 
+                type: DataTypes.STRING,
+            
+            },
+    
+          created_at: { //createdAt
+            allowNull: false,
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+          },
+       },
+		{
+			DataTypes,
+			timestamps: false,
+			modelName: 'otp',
+		}
+	);
+	return otp;
+};
