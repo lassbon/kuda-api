292ea905572b8e9a8370c7cf6b305703b8136f6d
Your Name <you@example.com>
2022-11-02 10:35:57 +0100

    Rearranged Table Customers

--- migrations/customer.js
+++ migrations/customer.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('customers', {
+    await queryInterface.createTable('customer', {
         sn: { 
         type: Sequelize.INTEGER,
         autoIncrement: true,
