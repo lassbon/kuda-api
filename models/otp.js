2a6c3c4a62d6aa5976957bf8ecaab979dde09e1a
Your Name <you@example.com>
2022-11-02 16:32:35 +0100

    Updated Migrations and models

---
+++ models/otp.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+const { DataTypes  } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => 
+{
+  const otp = sequelize.define('otp', 
+  {
+    sn: { 
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    
+    },
+    customer_id: { 
+        type: DataTypes.STRING,    
+    },
+    otp: { 
+        type: DataTypes.STRING,
+    
+    },
+    phone: { 
+        type: DataTypes.STRING,
+    
+    },
+    email: { 
+        type: DataTypes.STRING,
+    
+    },
+
+createdAt: { //createdAt
+    allowNull: false,
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+}, {});
+
+ 
+  return otp;
+
+
+}
