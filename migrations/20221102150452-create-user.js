4f61ad8c7885bd9c502d742a1bba0e02a840005f
Your Name <you@example.com>
2022-11-02 16:30:45 +0100

    Updated Migrations and models

--- migrations/20221102150452-create-user.js
+++ migrations/20221102150452-create-user.js
@@ -3,20 +3,153 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
+      sn: { 
+        type: DataTypes.INTEGER,
         autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+        unique: true
       },
-      firstName: {
-        type: Sequelize.STRING
+      customer_id: {
+          type: DataTypes.STRING,
+          primaryKey: true
       },
-      lastName: {
-        type: Sequelize.STRING
+      title: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      othernames: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
       },
       email: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING,
+        allowNull: false,
+        uniqueKey:true
+      },
+      is_email_verified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      phone_number: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          defaultValue: null
+      },
+      is_phone_number_verified: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+      },
+      gender: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      house_number: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      street: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      landmark: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      local_govt: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      dob: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      nin: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      is_nin_verified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: null
+      },
+      bvn: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: null
+       },
+      is_bvn_verified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: null
+       },
+    country: {
+        type: DataTypes.STRING,
+        defaultValue: null
+    },
+        state_origin: {
+            type: DataTypes.STRING,
+            defaultValue: null
+        },
+        local_govt_origin: {
+            type: DataTypes.STRING,
+            defaultValue: null
+        },
+        means_of_id: {
+            type: DataTypes.STRING,
+            defaultValue: null
+        },
+        means_of_id_number: {
+            type: DataTypes.STRING,
+            defaultValue: null
+        },
+        is_means_of_id_number_verified: {
+            type: DataTypes.STRING,
+            defaultValue: null
+        },
+        photo: {
+            type: DataTypes.STRING,
+            defaultValue: null
+        },
+        marital_status: {
+            type: DataTypes.STRING,
+            defaultValue: null
+        },
+      password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password_salt: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+        is_disable: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        is_disable_reason: {
+            type: DataTypes.TEXT,
+            defaultValue: null
+        },
+        createdAt: { //createdAt
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: { //updatedAt
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
       },
       createdAt: {
         allowNull: false,
