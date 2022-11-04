b5d311f1b0311c5f822c5c07caea1bd72b0d3652
Your Name <you@example.com>
2022-11-04 14:45:55 +0100

    Puttion Validation(JOI)

--- validations/validatejoi.js
+++ validations/validatejoi.js
@@ -1,9 +1,10 @@
 const Joi = require('joi');
 
 const schema = Joi.object({
-    lastname: Joi.string().alphanum().min(2).max(30).required(),
-    othernames: Joi.string().alphanum().min(2).max(30).required(),
+    surname: Joi.string().min(2).required(),
+    othernames: Joi.string().min(2).required(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    phone: Joi.string().min(2).required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-    repeat_password: Joi.ref('password'),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    repeat_password: Joi.ref('password')
 })
