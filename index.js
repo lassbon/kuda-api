5a8644bdcb32f80608b36c7d67a1b2807eefc884
Bilqisu Ismail <ismail.bilkisu@gmail.com>
2022-11-04 13:46:46 +0100

    dont know what Iam doing

---
+++ index.js
@@ -0,0 +1,10 @@
+const express =require('express')
+const {sequelize} = require('./models')
+const app = express()
+
+// app.post('/', (req,res)=>{})
+// //create the DB
+// async function main(){
+//     await sequelize.sync({force : true})
+// }
+// main()
