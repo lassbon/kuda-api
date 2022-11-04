41d77c585dde2aadbac15f874684188755c725f8
Your Name <you@example.com>
2022-11-04 15:32:53 +0100

    Updated The Otp to Otps && added the Harshing in Utils

--- migrations/otps.js
+++ migrations/otps.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('otp', {
+    await queryInterface.createTable('otps', {
         sn: { 
             type: Sequelize.INTEGER,
             autoIncrement: true,
