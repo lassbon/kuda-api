60a6d8e8c594b1e310a91febd573b608ab321247
Your Name <you@example.com>
2022-11-04 15:32:05 +0100

    Updated The Otp to Otps && added the Harshing in Utils

---
+++ utils/index.js
@@ -0,0 +1,68 @@
+
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
+const  hashMyPassword = (password) => {
+
+    return new Promise ((resolve, reject)  => {
+       
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash)  => {
+                if (err) {
+                    reject(err)
+                }
+                resolve([hash, salt])
+            })
+        })
+           
+        })
+}
+
+
+
+const generateOtp = () => {
+
+    const otp = Math.floor(1000 + Math.random() * 9000)
+
+    return otp
+}
+
+
+
+
+module.exports ={ hashMyPassword, generateOtp }
+
+
+
+// const bcrypt = require('bcrypt')
+// const saltRounds = 10
+
+// const  hashMyPassword = (password) => {
+
+//     return new Promise ((resolve, reject)  => {
+       
+//         bcrypt.genSalt(saltRounds, (err, salt) => {
+//             bcrypt.hash(password, salt, (err, hash)  => {
+//                 if (err) {
+//                     reject(err)
+//                 }
+//                 resolve([hash, salt])
+//             })
+//         })
+           
+//         })
+// }
+
+
+
+// const generateOtp = () => {
+
+//     const otp = Math.floor(1000 + Math.random() * 9000)
+
+//     return otp
+// }
+
+
+
+
+// module.exports ={  hashMyPassword, generateOtp }
