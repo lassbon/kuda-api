716608c17f3195aa50ac832b0d1bb73255bb4557
Your Name <you@example.com>
2022-11-04 13:06:14 +0100

    Updated Config

---
+++ config/sequelize.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+
+
+    const sequelize = new Sequelize(
+        process.env.DATABASE_NAME,
+        process.env.DATABASE_USER,
+        process.env.DATABASE_PASSWORD,
+        {
+            host: process.env.HOST,      
+            port: process.env.DATABASE_PORT,
+            dialect: 'mysql' /* one of | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+        }
+    )
+         
+
+
+module.exports = sequelize
