4638b4bbf0d82fcf7ac8c8932ab8d077e5d04599
Your Name <you@example.com>
2022-11-04 14:58:23 +0100

    Updated Validation(JOI)

--- validations/validatejoi.js
+++ validations/validatejoi.js
@@ -1,10 +1,15 @@
 const Joi = require('joi');
 
-const schema = Joi.object({
-    surname: Joi.string().min(2).required(),
-    othernames: Joi.string().min(2).required(),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
-    phone: Joi.string().min(2).required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-    repeat_password: Joi.ref('password')
-})
+const registervalidation = (data)=> {
+    const schema = Joi.object({
+        surname: Joi.string().min(2).required(),
+        othernames: Joi.string().min(2).required(),
+        email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+        phone: Joi.string().min(2).required(),
+        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+        repeat_password: Joi.ref('password')
+    })
+    return schema.validate(data)
+}
+
+module.exports = registervalidation
