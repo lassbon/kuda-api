40598d868641aec1dc52e22ad73c6f4153d43278
Your Name <you@example.com>
2022-11-06 22:20:41 +0100

    Added Sendgrid KeyCode

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class user extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({sn: { 
+  user.init({sn: { 
     type: DataTypes.INTEGER,
     autoIncrement: true,
     unique: true
@@ -165,5 +165,5 @@ country: {
     sequelize,
     modelName: 'User',
   });
-  return User;
+  return user;
 };
