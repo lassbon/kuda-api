1f4aa0a1769b89d4a4a1097105bebd091db4aff1
unknown <femisolaalawiye@gmail.com>
2022-11-07 11:41:14 +0100

    service platform installed

--- controllers/customer.controllers.js
+++ controllers/customer.controllers.js
@@ -1,3 +1,101 @@
+const {registerValidation}  = require('../validations/register.validation')
+const { customer, otp } = require('../models');
+const { Op } = require("sequelize");
+const { v4: uuidv4 } = require('uuid')
+const { hashMyPassword, generateOtp } = require('../utils')
+const { sendEmail } = require('../services/email')
+
+
+
+
+
+const register = (req, res) => { 
+    // Do something
+    const { error, value } = registerValidation(req.body)
+
+    if (error != undefined) {
+      
+        res.status(400).json({
+            status: false,
+            message: error.details[0].message
+        })
+    }else{
+    
+     console.log("alas: " ,JSON.stringify(req.body))
+    const { surname, othernames, email, phone, password, repeat_password } = req.body;
+        const customer_id = uuidv4()
+        const _otp = generateOtp()
+    try {
+
+    customer.findAll({
+        where: {
+            [Op.or]: [
+                { email: email },
+                { phone_number: phone }
+            ]
+        },
+        attributes: ['customer_id', 'title', 'email', 'phone_number']
+    })
+    .then((data) => {
+        if (data.length > 0) throw new Error('Email or phone number already exist')
+
+        return hashMyPassword(password) //[hash, salt]
+
+    })
+    .then((data2) => {
+
+        return customer.create({
+            customer_id: customer_id,
+            lastname: surname,
+            othernames: othernames,
+            email: email,
+            phone_number: phone,
+            password_hash: data2[0],
+            password_salt: data2[1],
+        })
+
+    })
+    .then((insertIntoOtpTable) => {
+
+     
+        return otp.create({
+            customer_id: customer_id,
+            otp: _otp,
+            email: email,
+            phone: phone,
+            email: email
+        })
+
+    })
+        .then((data3) => {
+        
+        sendEmail(email, 'OTP', ` Hello  ${surname} ${othernames},\n Your OTP is ${_otp}`)
+
+        res.status(200).send({
+            status: true,
+            message: 'Registration successful, An otp has been sent to your email'
+        })
+
+    })
+    .catch((err) => {
+           
+        res.status(400).json({
+            status: false,
+            message: err.message || "Some error occurred while creating the Customer."
+        })
+
+    })
+
+    } catch (error) {
+        res.status(400).json({
+            status: false,
+            message: error.message || "Some error occurred while creating the Customer."
+        })
+    }
+}
+}
+
+
 const verifyEmailOtp = (req, res) => { 
 
     const { email, _otp } = req.params
