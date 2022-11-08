34dfe9b722a30c4ef1b194507f02013c993cdb89
unknown <femisolaalawiye@gmail.com>
2022-11-08 22:05:37 +0100

    updated env

--- migrations/otp_20221031.js
+++ migrations/otp_20221031.js
@@ -30,7 +30,7 @@ module.exports = {
         
         },
 
-      created_at: { //createdAt
+      createdAt: { //createdAt
         allowNull: false,
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
