aa2dc367fbbf5a5219ade910b1f069d1e6aa622a
Your Name <you@example.com>
2022-11-09 18:53:01 +0100

    Rebuilt service file

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,17 +1,15 @@
+require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
 
 const sequelize = new Sequelize(
+    process.env.DATABASE_USER, 
     process.env.DATABASE_NAME,
-    process.env.DATABASE_USER,
     process.env.DATABASE_PASSWORD,
     {
-        host: process.env.HOST,      
+        host: process.env.DATABASE_HOST,
         port: process.env.DATABASE_PORT,
-        dialect: 'mysql' /* one of | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-    }
-)
-        
-
+        dialect:  'mysql' //| 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */ 
+    })
 
 module.exports = sequelize
