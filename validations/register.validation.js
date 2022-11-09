0a65db0a2c636d08a98aaa8ab62d0d0cbc966f08
Your Name <you@example.com>
2022-11-09 15:48:27 +0100

    Rebuilt service file

--- validations/register.validation.js
+++ validations/register.validation.js
@@ -7,12 +7,12 @@ const registerValidation = (data) => {
 
     const schema = Joi.object({
 
-        surname: Joi.string().min(2).required(),
-        othernames: Joi.string().min(2).required(),
-        email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
-        phone: Joi.string().min(2).required(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        repeat_password: Joi.ref('password')
+      surname: Joi.string().min(2).required(),
+      othernames: Joi.string().min(2).required(),
+      email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+      phone: Joi.string().min(2).required(),
+      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+      repeat_password: Joi.ref('password')
 
   
     })
