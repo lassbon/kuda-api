08856d2295638cc908f51bad3c9aa231149b38ec
deewan3696 <mail_d1@yahoo.com>
2022-11-11 14:40:13 +0100

    update

--- controllers/customer.controllers.js
+++ controllers/customer.controllers.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const {registerValidation}  = require('../validations/register.validation')
 const { customer, otp } = require('../models');
 const { Op } = require("sequelize");
@@ -97,8 +98,89 @@ const register = (req, res) => {
 }
 }
 
+const verifyEmailOtpAndSendPhoneOtp = (req, res) => { 
+
+    const phone_otp = generateOtp()
+
+    const { email, phone, _otp } = req.params
+    try {
+        otp.findAll({
+            where: {
+                email: email,
+                otp: _otp
+            },
+            attributes: [ 'otp', 'email', 'phone', 'createdAt'], 
+        })
+        .then((otpDataFetched) => {
+            if (otpDataFetched.length == 0) throw new Error('Invalid OTP')
+    
+            console.log("otpdataFetched: ", otpDataFetched[0])
+
+            const timeOtpWasSent = Date.now() - new Date(otpDataFetched[0].dataValues.createdAt)
+        
+            const convertToMin = Math.floor(timeOtpWasSent / 60000) // 60000 is the number of milliseconds in a minute
+
+            if (convertToMin > process.env.OTPExpirationTime) throw new Error('OTP has expired')
+
+            return customer.update({ is_email_verified: true }, {
+                where: {
+                  email: email
+                }
+              })
+            
+
+        
+        })
+        .then((emailverifiedData) => { 
+            return otp.destroy({
+                where: {
+                    otp: _otp,
+                    email: email
+                }
+            })
+        })
+        .then((data3) => { 
+
+            return otp.create({
+                otp: phone_otp,
+                phone: phone,
+                email: email
+            })
+           
+        })
+        .then((data4) => {
+            
+                   //send an otp to the phone number
+            sendSms(phone, `Hello, your otp is ${phone_otp}`)
+            sendEmail(email, 'Email Verification Successful', ` Hello  ${email},\n Thank you for verifying your email. An otp has been sent to your phone number, kindly use this otp to also verify your phone number`)
+         
+        })
+        .then((data5) => {
+                res.status(200).send({
+                    status: true,
+                    message: 'Email verification successful, An otp has been sent to your phone number'
+                })
+        })
+        .catch((err) => {
+            res.status(400).json({
+                status: false,
+                message: err.message || "Some error occurred while verifying OTP."
+            })
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            status: false,
+            message: err.message || "Some error occurred while verifying OTP."
+        })
+
+    }
+
+}
 
 
 
 
-module.exports = { register , verifyEmailOtp , sendEmail}
+
+
+module.exports = { register , verifyEmailOtpAndSendPhoneOtp , sendEmail}
