079ee317e40b39387a4e5603e91c4ae9da7de83e
Your Name <you@example.com>
2022-11-11 17:14:44 +0100

    Re-corrected The Connection for sequelize

--- controller/customer.js
+++ controller/customer.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { registerValidation }  = require('../validations/registervalidation')
+const { registerValidation }  = require('../validations/validatejoi')
 const { customer, otp } = require('../models');
 const { Op } = require("sequelize");
 const { v4: uuidv4 } = require('uuid')
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.APP_PORT
-const  sequelize  = require('./config/sequelize')
+const port = process.env.APPPORT
+//const  sequelize  = require('./config/sequelize')
 const bodyParser = require('body-parser')
 const registerRoute = require('./route/customer')
 
@@ -13,12 +13,12 @@ app.use(registerRoute)
 
 app.listen(port, () => {
 
-    sequelize.authenticate()
-    .then(sucessData => {
-            console.log('Connection has been established successfully.');
-    })
-    .catch(error => {
-            console.error('Unable to connect to the database:', error);
-    })
+//     sequelize.authenticate()
+//     .then(sucessData => {
+//             console.log('Connection has been established successfully.');
+//     })
+//     .catch(error => {
+//             console.error('Unable to connect to the database:', error);
+//     })
     
 })
