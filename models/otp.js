553c8b7b51c893388e8d3fdccc0c79d9ce081877
deewan3696 <mail_d1@yahoo.com>
2022-11-11 14:54:44 +0100

    my _seup_deewan now up to date

--- models/otp.js
+++ models/otp.js
@@ -6,41 +6,17 @@ module.exports = (sequelize, DataTypes) => {
 	class otp extends Model {
 		
 	}
-	customer.init(
-        {
-            sn: { 
-                type: DataTypes.INTEGER,
-                autoIncrement: true,
-                primaryKey: true,
-            
-            },
-            customer_id: { 
-                type: DataTypes.STRING,    
-            },
-            otp: { 
-                type: DataTypes.STRING,
-            
-            },
-            phone: { 
-                type: DataTypes.STRING,
-            
-            },
-            email: { 
-                type: DataTypes.STRING,
-            
-            },
-    
-          created_at: { //createdAt
-            allowNull: false,
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-          },
-       },
-		{
-			DataTypes,
-			timestamps: false,
-			modelName: 'otp',
-		}
-	);
-	return otp;
-};
+    otp.init({
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true
+       }, 
+        otp: DataTypes.STRING,
+        phone: DataTypes.STRING,
+        email:  DataTypes.STRING
+      }, {
+        sequelize,
+        modelName: 'otp',
+      });
+      return otp;
+    };
--- routes/customer_route.js
+++ routes/customer_route.js
@@ -4,3 +4,16 @@ const { register } = require('../controllers/customer.controllers')
 
 // @route   GET api/customers
 router.post('/register', register)
+
+router.get('/verify-email-otp/:_otp/:email/:phone', verifyEmailOtpAndSendPhoneOtp)
+
+router.get('/verify-phone-otp/:phone_otp/:phone', verifyPhoneOtp)
+
+router.post('/resend-phone-otp/:phone', resendPhoneOtp)
+
+router.post('/resend-email-otp/:email', resendEmailOtp)
+
+
+
+
+module.exports = router
