33d43c0817ec7a9fc77972c6d07887157af02e7f
Your Name <you@example.com>
2022-11-11 12:37:59 +0100

    Went Back And Redo My Creation Of DB

--- route/customer.js
+++ route/customer.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { register, verifyEmailOtp } = require('../controller/customer')
+const { register, verifyEmailOtpAndSendPhoneOtp } = require('../controller/customer')
 
 // @route   POST api/customers
 router.post('/register', register)
 
 
 
-router.get('/verify-email-otp/:_otp/:email', verifyEmailOtp)
+router.get('/verify-email-otp/:_otp/:email', verifyEmailOtpAndSendPhoneOtp)
 
 
 
