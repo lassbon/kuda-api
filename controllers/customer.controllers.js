1c086134f56f0bbcba1c26e36e4fa7411690d731
deewan3696 <mail_d1@yahoo.com>
2022-11-11 14:30:04 +0100

    update on controllers

--- controllers/customer.controllers.js
+++ controllers/customer.controllers.js
@@ -97,67 +97,6 @@ const register = (req, res) => {
 }
 }
 
-const verifyEmailOtp = (req, res) => { 
-
-    const { email, _otp } = req.params
-    try {
-        otp.findAll({
-            where: {
-                email: email,
-                otp: _otp
-            }
-        })
-        .then((otpDataFetched) => {
-            if (otpDataFetched.length == 0) throw new Error('Invalid OTP')
-    
-            const timeOtpWasSent = Date.now() - new Date(otpDataFetched[0].dataValues.createdAt)
-        
-            const convertToMin = Math.floor(timeOtpWasSent / 60000) // 60000 is the number of milliseconds in a minute
-
-            if (convertToMin > OTPEsperationTime) throw new Error('OTP has expired')
-
-            return customer.update({ is_email_verified: true }, {
-                where: {
-                  email: email
-                }
-              })
-            
-
-        
-        })
-        .then((emailverifiedData) => { 
-            return otp.destroy({
-                where: {
-                    otp: _otp,
-                    email: email
-                }
-            })
-        })
-        .then((data3) => { 
-
-            res.status(200).send({
-                status: true,
-                message: 'Email verification successful'
-            })
-
-        })
-        .catch((err) => {
-            res.status(400).json({
-                status: false,
-                message: err.message || "Some error occurred while verifying OTP."
-            })
-        })
-        
-    } catch (err) {
-        res.status(400).json({
-            status: false,
-            message: err.message || "Some error occurred while verifying OTP."
-        })
-
-    }
-
-}
-
 
 
 
