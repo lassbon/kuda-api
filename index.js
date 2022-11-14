bf711e3f83d766b019482e539465480bf7f6f098
deewan3696 <mail_d1@yahoo.com>
2022-11-14 09:30:55 +0100

    Rearranged Connection Of DB

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.APP_PORT
-const  sequelize  = require('./config/sequelize')
+//const  sequelize  = require('./config/sequelize')
 const bodyParser = require('body-parser')
 const displayRoutes = require('express-routemap');
 const registerRoute = require('./routes/customer.route')
@@ -13,13 +13,13 @@ app.use(registerRoute)
 
 app.listen(port, () => {
 
-    sequelize.authenticate()
-    .then(sucessData => {
-            console.log('Connection has been established successfully.');
-    })
-    .catch(error => {
-            console.error('Unable to connect to the database:', error);
-    })
+//     sequelize.authenticate()
+//     .then(sucessData => {
+//             console.log('Connection has been established successfully.');
+//     })
+//     .catch(error => {
+//             console.error('Unable to connect to the database:', error);
+//     })
     
 })
 
