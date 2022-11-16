afaf8a51dd6bbbb180b835656c3644b1a66b4598
Zubs <zubairay@gmail.com>
2022-11-16 15:17:55 +0100

    getting started

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.APP_PORT
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json())
+
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
