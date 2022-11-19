603a2a1ad800b481ee5ee5ad58d6a5bf2ee59554
Your Name <you@example.com>
2022-11-19 10:12:44 +0100

    Created The Authentification folder

--- route/customer.js
+++ route/customer.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { register, verifyEmailOtpAndSendPhoneOtp, verifyPhoneOtp, resendPhoneOtp, resendEmailOtp, updateCustomer} = require('../controller/customer')
 const {login} = require('../controller/Login')
+const {authorization} = require('../middlewares/authentification')
 // @route   POST api/customers
 router.post('/register', register)
 
@@ -15,7 +16,9 @@ router.post('/resend-email-otp/:email', resendEmailOtp)
 
 router.put('/update/:customer_id', updateCustomer)
 
-router.put('/update/:customer_id', login)
+router.get('/login/:email/:password',authorization, login)
+
+
 
 
 
