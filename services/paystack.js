df8a080e315e74f9b9e532f365de0866e0aa8001
Your Name <you@example.com>
2022-12-13 07:46:07 +0100

    Working On Outside Call To Paystack

---
+++ services/paystack.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const axios = require('axios')
+const baseUrl = process.env.PAYSTACK_BASE_URL
+
+
+// const intializePayment =  ({amount, email}) => {  destructed the email and amount from the req.body, but femisola preffered it the other way
+   
+const initializePayment = (amount, email) => { 
+
+      return  axios({
+            method: 'post',
+            url: `${baseUrl}/transaction/initialize`,
+            headers: {
+                Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+                'Content-Type': 'application/json'
+            },
+            data: {
+                amount: amount * 100, //amount has to be in kobo, according to paystack
+                email: email
+            }
+        })
+}
+
+
+
+const verifyPayment = (ref) => { 
+
+    return axios({
+        method: 'get',
+        url: `${baseUrl}/transaction/verify/${ref}`,
+        headers: {
+            Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+            'Content-Type': 'application/json'
+        },
+    })
+}
+
+const getBank = () => { 
+
+    return axios({
+        method: 'get',
+        url: `${baseUrl}/bank`,
+        headers: {
+            Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+            'Content-Type': 'application/json'
+        },
+    })
+}
+
+
+
+module.exports = { initializePayment, verifyPayment, getBank }
